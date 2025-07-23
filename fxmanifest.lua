f358fdc7f130af3c4c93dc3be88bcafd18816150
ZykeWasTaken <linussnilsson03@gmail.com>
2025-07-24 00:28:34 +0200

    feat(new loader)

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,12 +4,17 @@ author "discord.gg/zykeresources"
 lua54 "yes"
 version "1.0.3"
 
-shared_scripts {
-    "@zyke_lib/imports.lua",
+shared_script "@zyke_lib/imports.lua"
+
+files {
+    "client.lua",
     "config.lua",
+    "locales/*.lua",
 }
 
-client_script "client.lua"
-file "locales/*.lua"
+loader {
+    "client.lua",
+    "shared:config.lua",
+}
 
 dependency "zyke_lib"
